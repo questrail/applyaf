a183940e09123eb4f11d8972e6dd321d0715c6f5
Matthew Rankin <matthew@questrail.com>
2021-12-21 19:31:49 -0600

    Add version back into __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013-2017 The applyaf developers. All rights reserved.
+# Project site: https://github.com/questrail/applyaf
+# Use of this source code is governed by a MIT-style license that
+# can be found in the LICENSE.txt file for the project.
+""" Applyaf
+"""
+__version__ = '1.2.0'
