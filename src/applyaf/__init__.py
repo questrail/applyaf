4228c1239dafe256d8ab42010a4f5a97954300c8
Matthew Rankin <369937+matthewrankin@users.noreply.github.com>
2026-08-31 13:54:05 -0500

    Update the copyright notices through 2026
    Bump the range in LICENSE.txt and in the source and test file headers.

--- src/applyaf/__init__.py
+++ src/applyaf/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013-2024 The applyaf developers. All rights reserved.
+# Copyright (c) 2013-2026 The applyaf developers. All rights reserved.
 # Project site: https://github.com/questrail/applyaf
 # Use of this source code is governed by a MIT-style license that
 # can be found in the LICENSE.txt file for the project.
